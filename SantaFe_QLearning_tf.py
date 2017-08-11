8ac1af6347ff35fafaf95e90eccb9b609ed436ac
Linda MacPhee-Cobb <timestocome@gmail.com>
2017-08-11 15:07:21 -0500

    Replace SantaFe Maze Python QLearning with TensorFlow version

---
+++ SantaFe_QLearning_tf.py
@@ -0,0 +1,204 @@
+
+
+# http://github.com/timestocome
+
+
+# based on this tutorial:
+# Q Table Learning
+# https://medium.com/emergent-future/simple-reinforcement-learning-with-tensorflow-part-0-q-learning-with-tables-and-neural-networks-d195264329d0
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import animation
+
+
+# set up
+np.set_printoptions(threshold=4096)
+
+
+####################################################################################
+# set up data
+# the SantaFe Ant Trail Maze
+# https://en.wikipedia.org/wiki/Santa_Fe_Trail_problem
+####################################################################################
+
+maze = np.asarray([
+        1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 1, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 
+], dtype='float32')
+
+maze = maze.reshape(32, 32)
+rewards_in_maze = np.sum(maze)
+
+
+# maze utility functions
+# moves take place in 2d array, q is flattened version of it
+up = 0
+def move_up(q):
+    z = q - maze.shape[1]
+    if z // maze.shape[1] < 0: return q
+    else: return z
+    return q
+   
+
+down = 1
+def move_down(q):
+    z = q + maze.shape[1]
+    if z // maze.shape[1] >= maze.shape[1] : return q
+    else: return z
+
+right = 2
+def move_right(q):
+    z = q + 1
+    if z > maze.shape[1]: return q
+    else: return z
+    
+
+left = 3
+def move_left(q):
+    z = q - 1
+    if z < 0: return q
+    else: return z
+
+
+
+
+
+# pretty print maze
+def print_maze(m):
+
+    m = m.reshape(maze.shape[0], maze.shape[1])
+    for i in range(maze.shape[0]):
+        for j in range(maze.shape[1]):
+            print(m[j, :])
+
+
+
+
+####################################################################################
+# Q Table Learning
+####################################################################################
+n_directions = 4
+Q = np.zeros((1024, n_directions))
+lr = .8
+discount_rate = .95
+n_episodes = 500
+rewards_list = []
+max_steps = 1024
+random_move = 3. * 1. / n_episodes
+
+
+# ant path through maze
+track_location = np.zeros(1024)
+
+
+
+
+
+for i in range(n_episodes):
+
+    # reset for each pass through maze
+    total_rewards = 0.
+    done = False
+    n_steps = 0
+    q = 0
+    game_maze = maze.copy().flatten()
+
+    # over write so only last path is saved
+    track_ant = np.zeros(1024)
+
+    
+    # Q Table learning
+    while n_steps < max_steps:
+        n_steps += 1
+
+        # choose greedy or random
+        random_probability = 1. - random_move * i # start totally random and decrease to greedy over time
+        if random_probability < 0.1: random_probability = 0.1   # bottom out random moves at 10%
+        if (np.random.random(1)) >  random_probability:  
+            direction = np.random.randint(0, 3)
+        else:       # greedy
+            direction = np.argmax(Q[q])
+
+    
+        # check state of environment and update 
+        # update ant location
+        old_state = q
+        if direction == 0: q = move_up(q)
+        if direction == 1: q = move_right(q)
+        if direction == 2: q = move_down(q)
+        if direction == 3: q = move_left(q)
+        new_state = q
+
+        # reward? 
+        reward = game_maze[new_state]
+        game_maze[q] = 0     # remove reward if found so ant doesn't keep revisiting same spot
+        
+        total_rewards += reward
+        #print("Run: %d Rewards collected: %d" % (i, np.sum(maze) - np.sum(game_maze))
+        
+        if total_rewards >= rewards_in_maze: 
+            done = True
+            break
+
+        Q[old_state, direction] = Q[old_state, direction] + lr * (reward + discount_rate * np.max(Q[new_state, :]) - Q[old_state, direction])
+        rewards_list.append(total_rewards)
+
+        #print("Final Q-Table Values:", Q)
+
+        # save ant location for animation
+        track_ant[new_state] += 1
+
+    print("Episode: %d Score: %d" % (i, total_rewards))
+
+    track_location = track_ant
+
+
+
+
+
+#################################################################################################
+# display last ant path
+################################################################################################
+from pylab import *
+
+
+track_location = track_location.reshape((32, 32))
+track_location += maze * 30     # show maze
+
+
+plt.figure(figsize=(10, 10))
+imshow(track_location, cmap='Blues', interpolation='nearest')
+grid(True)
+plt.show()
